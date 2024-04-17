6b24ecfd33d7189ebeaa2fc365db9081b4cad04d
Roberto Eduardo <robertoemd@gmail.com>
2024-04-17 15:32:23 -0300

    commit inicial

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const PORT = 8081;
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/htmls/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log("SERVIDOR RODANDO NA PORTA: " + PORT);
+});
